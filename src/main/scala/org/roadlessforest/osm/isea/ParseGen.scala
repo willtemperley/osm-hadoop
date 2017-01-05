7fbc656b6c8adfa9051d0f4ef6f1da83c64ca74d
will <willtemperley@gmail.com>
2017-01-05 18:52:42 +0100

    isea support

---
+++ src/main/scala/org/roadlessforest/osm/isea/ParseGen.scala
@@ -0,0 +1,114 @@
+package org.roadlessforest.osm.isea
+
+import java.io.File
+
+import com.vividsolutions.jts.geom.{Coordinate, GeometryFactory, Polygon}
+import org.roadlessforest.osm.shp.GeomType
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+
+
+/**
+  * Created by willtemperley@gmail.com on 19-Dec-16.
+  */
+object ParseGen {
+
+  class Acceptor() {
+
+    val geomFact = new GeometryFactory
+
+    var polyEnd = false
+
+    var fileEnd = false
+
+    val coords = new ArrayBuffer[Coordinate]
+
+    var prev: String = ""
+
+    var id: Int = _
+
+    def add(v: String): Unit = {
+
+      if (v.equals("END")) {
+        polyEnd = true
+        if (prev.equals("END")) {
+          fileEnd = true
+        }
+      }
+
+      prev = v
+
+      if (!polyEnd) {
+
+        val list = v.split(" ").toList
+
+        val head = list.head
+        val x = list(1).toDouble
+        val y = list(2).toDouble
+
+        if (head.length > 0) {
+          id = head.toInt
+        } else {
+          coords += new Coordinate(x, y)
+        }
+      }
+
+    }
+
+    def isCrossing: Boolean = {
+
+      for (coordPair <- coords.sliding(2)) {
+        val a = coordPair(0)
+        val b = coordPair(1)
+        if (Math.abs(a.x - b.x) > 180) {
+          println(id + " is crossing")
+          return true
+          //          return null
+        }
+      }
+      false
+    }
+
+    def getPolygon: Polygon = {
+
+      val poly = geomFact.createPolygon(coords.toArray)
+      polyEnd = false
+      coords.clear()
+      poly
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val gridName = "isea3h9"
+
+    val acceptor = new Acceptor
+    val f = new File("e:/isea3h/" + gridName + ".gen")
+
+    val s = Source.fromFile(f)
+    val lines = s.getLines()
+
+    val sw = new ShapeWriter(geomType = GeomType.Polygon)
+
+    for (elem <- lines) {
+      acceptor.add(elem)
+      if (!acceptor.fileEnd) {
+        if (acceptor.polyEnd) {
+          val isCrossing = acceptor.isCrossing
+
+          val p = acceptor.getPolygon
+          //          if (p != null) {
+//          println(p)
+          if (!isCrossing) sw.addFeature(p, Seq(acceptor.id.toString))
+          //          }
+        }
+      }
+    }
+
+    sw.write("e:/isea3h/shp/" + gridName + ".shp")
+
+  }
+
+}
---
+++ src/main/scala/org/roadlessforest/osm/isea/ParseKml.scala
@@ -0,0 +1,19 @@
+package org.roadlessforest.osm.isea
+
+import java.io.File
+
+/**
+  * Created by willtemperley@gmail.com on 05-Jan-17.
+  */
+class ParseKml {
+
+  def main(args: Array[String]): Unit = {
+
+    val r11 = new File("E:/tmp/isea3h11/r11.kml")
+
+
+
+
+  }
+
+}
